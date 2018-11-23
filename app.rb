b6744d16bb940e7060d159ac96b76445e7d13d4d
Stanislao777 <44646881+Stanislao777@users.noreply.github.com>
2018-11-23 17:25:39 -0400

    Test for heroku

--- app.rb
+++ app.rb
@@ -1,24 +1,29 @@
 require 'sinatra'
 require './lib/game'
 
-get "/" do
-    erb :welcome
-end
+class App < Sinatra::Base
 
-get '/homePage' do
-    erb :homePage
-end
+    get "/" do
+        erb :welcome
+    end
 
-get '/playWithFriends' do
-    width = 3
-    height = 3
-    $GAME = Game.new(width, height)
-    @game = $GAME
-    erb :playWithFriends
-end
+    get '/homePage' do
+        erb :homePage
+    end
 
-post '/playWithFriends' do
-    response = $GAME.pressBoard(params[:selection].to_i)
-    @game = $GAME
-    erb :playWithFriends
+    get '/playWithFriends' do
+        width = 3
+        height = 3
+        $GAME = Game.new(width, height)
+        @game = $GAME
+        erb :playWithFriends
+    end
+
+    post '/playWithFriends' do
+        response = $GAME.pressBoard(params[:selection].to_i)
+        @game = $GAME
+        erb :playWithFriends
+    end
+
+    run! if app_file == $0;
 end
