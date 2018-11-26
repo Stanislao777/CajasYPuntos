eb97e7eeb54a57eb140798a247cca5948817c271
Vargas Stanislao <e_vargas.v@outlook.com>
2018-11-25 23:49:37 -0400

    Anadiendo escenario jusgar con tres jugadores

--- features/startGame.feature
+++ features/startGame.feature
@@ -7,4 +7,11 @@ Feature: Jugar con amigos
             Given visito la pagina Elegir Jugadores
             When presiono el boton "2 jugadores"
             Then deberia ir a la pagina /playWithFriends con el titulo "Jugador 1: 0" 
+            And mostrar el titulo "Jugador 2: 0"
+
+        Scenario: Jugar con tres amigo
+            Given visito la pagina Elegir Jugadores
+            When presiono el boton "3 jugadores"
+            Then deberia ir a la pagina /playWithFriends con el titulo "Jugador 1: 0" 
             And mostrar el titulo "Jugador 2: 0" 
+            And mostrar el titulo "Jugador 3: 0"
--- features/step_definitions/startGame_steps.rb
+++ features/step_definitions/startGame_steps.rb
@@ -1,15 +1,17 @@
 Given("visito la pagina Elegir Jugadores") do
     visit('/playWithFriends')
-  end
+end
+
+When("presiono el boton {string}") do |string|
+  click_button('2 jugadores')
+end
+
+Then("deberia ir a la pagina \/playWithFriends con el titulo {string}") do |string|
+  expect(page).to have_content('Jugador 1: 0')
+end
+
+Then("mostrar el titulo {string}") do |string|
+  expect(page).to have_content('Jugador 2: 0')
+end
+
   
-  When("presiono el boton {string}") do |string|
-    click_button('2 jugadores')
-  end
-  
-  Then("deberia ir a la pagina \/playWithFriends con el titulo {string}") do |string|
-    expect(page).to have_content('Jugador 1: 0')
-  end
-  
-  Then("mostrar el titulo {string}") do |string|
-    expect(page).to have_content('Jugador 2: 0')
-  end
